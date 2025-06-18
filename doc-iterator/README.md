125010cf72c8850e5c769c57e56c17b38fe3acf2
fflorent <florent.git@zeteo.me>
2025-06-18 14:09:13 +0200

    Changes

---
+++ doc-iterator/README.md
@@ -0,0 +1,45 @@
+# Doc iterator
+
+**Status: ✅ Ready to be used in production**
+
+⚠️: Despite our efforts to avoid bugs in this script, please check your
+backups before running it, especially if you intent to use the `-w` option.
+
+This script allows you to run scripts on each Grist documents in your S3 bucket.
+
+## Use case
+
+It covers many use cases:
+ - you want to extract information about each documents (like the widgets being used);
+ - you want to vacuum the documents to reduce their size;
+ - ...
+
+## Usage
+
+See `./doc-iterator.sh --help`.
+
+### Example of use
+
+*vacuum.sh*
+```bash
+#!/usr/bin/env bash
+set -eEuo pipefail
+
+$SQLITE3 "$1" "vacuum"
+```
+
+*list-tables.sh*
+```bash
+#!/usr/bin/env bash
+set -eEuo pipefail
+
+doc_id=$(basename -s ".grist" "$1")
+$SQLITE3 "$1" ".tables" > "/tmp/${doc_id}.txt"
+```
+
+*To run it now on staging environment (given `staging-grist` is an alias registered in your minio client configuration):*
+```bash
+$ chmod u+x  ./vacuum.sh ./list-tables.sh
+$ bash ./doc-iterator.sh -w staging-grist/grist-preprod-grist/docs/ ./vacuum.sh ./list-tables.sh
+```
+
--- doc-iterator/doc-iterator.sh
+++ doc-iterator/doc-iterator.sh
@@ -6,14 +6,23 @@ set -eEu -o pipefail
 help=false
 write=false
 verbose=false
+scripts=()
 if [[ $# -eq 0 ]]; then
   help=true
 fi
 
-info() {
+debug() {
   if $verbose; then
-    echo "INFO: " "$@";
+    echo "DEBUG: " "$@"
   fi
+}
+
+info() {
+  echo "INFO: " "$@"
+}
+
+error() {
+  echo -e "\033[31mERROR:" "$@" "\033[0m"
 }
 
 while [[ $# -gt 0 ]]; do
@@ -36,52 +45,63 @@ while [[ $# -gt 0 ]]; do
       shift
       ;;
     *)
-      s3_path="${1/%\//}" # Remove trailing slash if present
+      if [ -z "${s3_path:-}" ]; then
+        s3_path="${1/%\//}" # Remove trailing slash if present
+      else
+        scripts+=("$1")
+      fi
       shift
-      scripts=("$@")
-      shift ${#scripts[@]}
       ;;
-    esac
-  done
+  esac
+done
 
-  if $help; then
-    echo "Usage: ./doc-iterator/doc-iterator.sh [OPTIONS] s3_path scripts..."
-    echo "Options:"
-    echo "  -h, --help      Show this help message and exit"
-    echo "  -w, --write     Push back changes to S3 if the document has changed"
-    echo "  -v, --verbose   Enable verbose output"
-    echo "  s3_path         The S3 path to the docs"
-    echo "  scripts         The scripts to run on the downloaded files"
-    echo ""
-    echo "Environment Variables:"
-    echo "  MINIO_MC       The MinIO client command (default: mc)"
-    echo "  SQLITE3        The SQLite3 command, exposed to each script (default: sqlite3)"
-    exit 0
-  fi
+if $help; then
+  echo "Usage: ./doc-iterator/doc-iterator.sh [OPTIONS] s3_path scripts..."
+  echo "Options:"
+  echo "  -h, --help            Show this help message and exit"
+  echo "  -w, --write           Push back changes to S3 if the document has changed"
+  echo "  -v, --verbose         Enable verbose output"
+  echo "  s3_path               The S3 path to the docs"
+  echo "  scripts               The scripts to run on the downloaded files"
+  echo ""
+  echo "Environment Variables:"
+  echo "  MINIO_MC       The MinIO client command (default: mc)"
+  echo "  SQLITE3        The SQLite3 command, exposed to each script (default: sqlite3)"
+  exit 0
+fi
 
 # Check if s3_path is set
 if [[ -z "${s3_path:-}" ]]; then
-  echo "Error: s3_path is required."
+  error "s3_path is required."
   exit 1
 fi
 
 # Check if scripts are set
 if [[ -z "${scripts:-}" ]]; then
-  echo "Error: No scripts provided."
+  error "No scripts provided."
   exit 1
 fi
 
 # Check if MINIO_MC is set, if not, set it to the default value
 MINIO_MC=${MINIO_MC:-"mc"}
-# if verbose is not set, make mc quiet
-if ! $verbose; then
-  MINIO_MC="$MINIO_MC --quiet"
-fi
+
+minio_retry() {
+  fibo=(0 1 2 3 5 8 13 21 34 55)
+  for i in "${fibo[@]}"; do
+    sleep "$i"
+    if $MINIO_MC "$@"; then
+      return 0
+    fi
+    debug "Problem occurred while running $MINIO_MC $*. Retry according to the fibonacci suit"
+  done
+  error "Failed to run command $MINIO_MC $*"
+  return 1
+}
 
 for script in "${scripts[@]}"; do
   # Check if the script exists and is executable
   if [[ ! -x "$script" ]]; then
-    echo "Error: Script $script does not exist or is not executable."
+    error "Script $script does not exist or is not executable."
     exit 1
   fi
 done
@@ -89,7 +109,7 @@ done
 # Check if we can list the S3 path
 
 # List all .grist files in the S3 path, except those that contain a ~
-files=$($MINIO_MC ls --json "$s3_path" | jq -r '.key | select(test("^[^~]+.grist$"))')
+files=$(minio_retry ls --json "$s3_path" | jq -r '.key | select(test("^[^~]+.grist$"))')
 
 dest_tmp_dir=$(mktemp -d)
 
@@ -98,23 +118,26 @@ for file in $files; do
   remote_path="$s3_path/$file"
   tmp_file="$dest_tmp_dir/$file"
   tmp_file_sha256="${tmp_file}.sha256"
-  $MINIO_MC get "$remote_path" "$tmp_file"
+  if ! minio_retry get "$remote_path" "$tmp_file" &>/dev/null; then
+    error "File does not exist, skip: $remote_path"
+  fi
   sha256sum "$tmp_file" > "$tmp_file_sha256"
 
   # Run each script on the downloaded file
   for script in "${scripts[@]}"; do
-    info "Running script $script on file $tmp_file"
-    SQLITE3=${SQLITE3:-"sqlite3"} $script "$tmp_file"
+    debug "Running script $script on file $tmp_file"
+    SQLITE3=${SQLITE3:-"sqlite3"} $script "$tmp_file" && \
+      info "✅ [SUCCESS] Successfully run script $script on $file" || \
+      error "❌ [USER_ERROR] User script error for $script on $file"
   done
 
   # If write is true and changes has been made, push the new file to S3
   if $write; then
-    if ! sha256sum -c "$tmp_file_sha256" --quiet; then
-      info "Changes detected in $tmp_file, pushing to S3"
-      # FIXME: Uncomment the following line to push the file to S3
-      # $MINIO_MC put "$tmp_file" "$remote_path"
+    if ! sha256sum -c "$tmp_file_sha256" &>/dev/null; then
+      info "⬆️ Changes detected in $tmp_file, pushing to S3"
+      minio_retry put "$tmp_file" "$remote_path" &>/dev/null
     else
-      info "No changes detected in $tmp_file, not pushing to S3"
+      debug "No changes detected in $tmp_file, not pushing to S3"
     fi
   fi
 

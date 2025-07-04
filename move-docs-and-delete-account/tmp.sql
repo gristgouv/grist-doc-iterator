9b84149fb3959ca4c1ddac4d8a3d0bbfbd888aa8
Vincent Viers <vincent.viers@beta.gouv.fr>
2025-07-04 11:40:39 +0200

    fix: wrong join query
    Unclear what the impact of this has been on production...

--- move-docs-and-delete-account/tmp.sql
+++ move-docs-and-delete-account/tmp.sql
@@ -1,87 +1,45 @@
-#!/usr/bin/env bash
-
-# Retrieve arguments passed to the command
-
-moved_suffix="__moved"
-
-for i in "$@"; do
-  case $i in
-    -s=*|--src=*)
-      src="${i#*=}"
-      shift
-      ;;
-    -d=*|--dst=*)
-      dst="${i#*=}"
-      shift
-      ;;
-    --moved_suffix=*)
-      moved_suffix="${i#*=}"
-      shift
-      ;;
-    *)
-      # unknown option
-      ;;
-  esac
-done
-
-echo_red() {
-  echo -e "\033[0;31m$1\033[0m"
-}
-
-echo_green() {
-  echo -e "\033[0;32m$1\033[0m"
-}
-
-if [ -z "$src" ] || [ -z "$dst" ]; then
-  echo_red "Usage: $0 --src=... --dst=... [--moved_suffix=...]"
-  exit 1
-fi
-
-cat_cmd=$(which bat batcat cat | head -n 1)
-declare -a cat_cmd_args
-if [[ $(basename "$cat_cmd") == "bat"* ]]; then
-  cat_cmd_args+=("-l" "sql" "-p" "--paging=never")
-fi
-
-echo_red "Account $src will be deleted"
-echo_green "Account $dst will receive the docs"
-echo ""
-read -p "Press enter to continue"
-echo ""
-
-$cat_cmd ${cat_cmd_args[@]} <<EOF
-DROP TABLE IF EXISTS source_info;
-
 CREATE TEMPORARY TABLE source_info as select u.id as user_id, w.id as workspace_id, o.id as org_id
 from logins l join users u on u.id=l.user_id
-  join orgs o on o.owner_id=u.id
-  join workspaces w on w.org_id=o.id
-where l.email='${src}' and o.name='Personal';
+join orgs o on o.owner_id=u.id
+join workspaces w on w.org_id=o.id
+where l.email='florian.delezenne@mail.numerique.gouv.fr' and o.name='Personal';
 
 select * from source_info;
+--  user_id | workspace_id | org_id 
+-- ---------+--------------+--------
+--     3331 |         3830 |   3398
+--     3331 |            4 |   3398
 
 DROP VIEW IF EXISTS target_info;
 
 CREATE TEMPORARY VIEW target_info as select u.id as user_id, o.id as org_id
 from logins l join users u on u.id=l.user_id
-  join orgs o on o.owner_id=u.id
-  join acl_rules ar on ar.org_id=o.id
-where l.email='${dst}' and o.name='Personal' and ar.permissions=63
+join orgs o on o.owner_id=u.id
+join acl_rules ar on ar.org_id=o.id
+where l.email='florian.delezenne@numerique.gouv.fr' and o.name='Personal' and ar.permissions=63
 LIMIT 1;
 
 select * from target_info;
+--  user_id | org_id 
+-- ---------+--------
+--    12760 |  13394
 
 begin;
 
-UPDATE workspaces set name=(name || '${moved_suffix}'), org_id=(SELECT t.org_id from target_info t) where id in (SELECT s.workspace_id from source_info s) returning *; -- Change workspace orgs and rename their name won't conflict with the target organization ones
+UPDATE workspaces set name=(name || '__moved'), org_id=(SELECT t.org_id from target_info t) where id in (SELECT s.workspace_id from source_info s) returning *; -- Change workspace orgs and rename their name won't conflict with the target organization ones
+--   id  |              name               |          created_at           |         updated_at         | org_id | removed_at 
+-- ------+---------------------------------+-------------------------------+----------------------------+--------+------------
+--  3830 | Home__moved                     | 2024-09-04 16:54:06.430292+00 | 2024-09-04 16:54:06.463+00 |  13394 | 
+--     4 | Homeold_betagouv_account__moved | 2023-05-31 08:11:42+00        | 2023-05-31 08:11:42.823+00 |  13394 | 
 
 WITH source_and_target_org_id_match as (
-  select src_grp.id as src_grp_id, dst_grp.id as dst_grp_id
-  from groups src_grp join groups dst_grp on src_grp.name=dst_grp.name
-    join acl_rules src_ar on src_grp.id=src_ar.group_id
-    join acl_rules dst_ar on dst_grp.id=dst_ar.group_id
-  where src_ar.org_id=(select distinct(org_id) from source_info) and dst_ar.org_id=(select distinct(org_id) from target_info)
+select src_grp.id as src_grp_id, dst_grp.id as dst_grp_id
+from groups src_grp join groups dst_grp on src_grp.name=dst_grp.name
+join acl_rules src_ar on src_grp.id=src_ar.group_id
+join acl_rules dst_ar on dst_grp.id=dst_ar.group_id
+where src_ar.org_id=(select distinct(org_id) from source_info) and dst_ar.org_id=(select distinct(org_id) from target_info)
 )
+
 UPDATE group_groups
 set subgroup_id=(select dst_grp_id from source_and_target_org_id_match where src_grp_id=subgroup_id)
 where subgroup_id in (select src_grp_id from source_and_target_org_id_match) returning *; -- Make moved workspace inherit their rights from the target org
@@ -104,4 +62,3 @@ DELETE from orgs where owner_id=(select distinct(user_id) from source_info);
 DELETE from users where id=(select distinct(user_id) from source_info);
 
 commit;
-EOF
